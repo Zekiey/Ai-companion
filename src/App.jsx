b92006ab6ea82f8725ee49bc42ada076782e64f0
Adeola Olukanni <adeolaolukanni20@gmail.com>
2024-01-08 22:34:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ const App = () => {
 
 
   return (
-    <div className='min-h-screen bg-[#0B0B0D] h-[480vh]'>
-
-      <Header />
+    <div className='min-h-screen bg-[#0B0B0D] h-[480vh] '>
+ <div className='box-border'><Header /></div>
+      
 
 
 
